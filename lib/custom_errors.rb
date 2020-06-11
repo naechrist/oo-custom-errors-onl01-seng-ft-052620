354178a7c2ecc2b7726aef3e620a6bc60d830177
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-11 18:45:28 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -15,7 +15,7 @@ class Person
   end
   class PartnerError < StandandError
     def message 
-      
+      "you must give the get_married method an argument of an instance of the person class!"
     end
   end
 end
